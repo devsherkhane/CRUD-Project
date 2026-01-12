15b24aa35605a33a303f414224ee952d248fcd34
Dev <devsherkhane6@gmail.com>
2026-01-12 14:22:00 +0530

    Initial commit

---
+++ db.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+var DB *sql.DB
+
+func ConnectDB() {
+	var err error
+
+	dsn := "root:9595520628@@tcp(127.0.0.1:3306)/student_db"
+	DB, err = sql.Open("mysql", dsn)
+
+	if err != nil {
+		log.Fatal("DB open error:", err)
+	}
+
+	err = DB.Ping()
+	if err != nil {
+		log.Fatal("DB connection error:", err)
+	}
+
+	fmt.Println("✅ MySQL Connected")
+}
---
+++ frontend/src/data/locations.js
@@ -0,0 +1,10 @@
+export const locations = {
+  "Maharashtra": {
+    "Pune": ["Haveli", "Pune City", "Maval", "Mulshi"],
+    "Nashik": ["Baglan", "Chandwad", "Deola"]
+  },
+  "Gujarat": {
+    "Ahmedabad": ["Bavla", "Daskroi", "Detroj-Rampura"],
+    "Surat": ["Bardoli", "Choryasi", "Mahuva"]
+  }
+}
